2b9165ebdd818375cb4e9deb64fa3d1982b23a6b
Aman <amanraikwar1996@gmail.com>
2026-02-11 19:00:57 +0530

    wrote functions for serve next and reset queue for AdminScreen.jsx

--- wait-wise/src/components/AdminScreen.jsx
+++ wait-wise/src/components/AdminScreen.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+function AdminScreen({ queue, setQueue, setTokenCounter, setServiceStartTime }) {
 
-const AdminScreen = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  const serveNext = () => {
+    if (queue.length === 0) {
+      alert("No tokens in queue");
+      return;
+    }
+
+    setQueue(queue.slice(1));
+    setServiceStartTime(Date.now());
+  };
+
+  const resetQueue = () => {
+    if (!window.confirm("Reset queue?")) return;
+
+    setQueue([]);
+    setTokenCounter(1);
+    localStorage.removeItem("currentUserToken");
+  };
+
+  return()
+
 }
 
 export default AdminScreen;
